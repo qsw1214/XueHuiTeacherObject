ac077a2f1393bade04b85071fdcbdadcda8cf1e9
yaolizhi <yaolizhi@aliyun.com>
2018-01-19 11:45:51 +0800

    添加发布作业功能

---
+++ daycareParent/daycareParent/智慧校园/家庭作业/ViewController/XHAssignmentHomeWorkViewController.h
@@ -0,0 +1,18 @@
+//
+//  XHAssignmentHomeWorkViewController.h
+//  daycareParent
+//
+//  Created by Git on 2018/1/19.
+//  Copyright © 2018年 XueHui. All rights reserved.
+//
+
+#pragma mark 家庭作业->发布作业
+
+
+
+
+#import "BaseViewController.h"
+
+@interface XHAssignmentHomeWorkViewController : BaseViewController
+
+@end
---
+++ daycareParent/daycareParent/智慧校园/家庭作业/ViewController/XHAssignmentHomeWorkViewController.m
@@ -0,0 +1,30 @@
+//
+//  XHAssignmentHomeWorkViewController.m
+//  daycareParent
+//
+//  Created by Git on 2018/1/19.
+//  Copyright © 2018年 XueHui. All rights reserved.
+//
+
+#import "XHAssignmentHomeWorkViewController.h"
+
+@interface XHAssignmentHomeWorkViewController ()
+
+@end
+
+@implementation XHAssignmentHomeWorkViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setNavtionTitle:@"发布作业"];
+    [self setItemContentType:NavigationTitleType withItemType:NavigationItemRightype withIconName:nil withTitle:@"记录"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
