6d601d6eed8340d98b468b82350abd16ec050870
姚立志 <yaolizhi@aliyun.com>
2018-04-10 09:06:50 +0800

    添加路由跳转第三方框架

---
+++ daycareParent/daycareParent/智慧校园/班级管理/ViewController/XHClassManageViewController.h
@@ -0,0 +1,15 @@
+//
+//  XHClassManageViewController.h
+//  daycareParent
+//
+//  Created by mac on 2018/4/9.
+//  Copyright © 2018年 XueHui. All rights reserved.
+//
+
+#pragma mark 班级管理
+
+#import "BaseViewController.h"
+
+@interface XHClassManageViewController : BaseViewController
+
+@end
